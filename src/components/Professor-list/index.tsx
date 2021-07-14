ce20f9cc1e62b7c19eb7b4236472d63b32f73b37
Pedro <pedropaulo3521@gmail.com>
2021-07-14 17:59:28 -0300

    Adicionado foto a lista de tutores

--- src/components/Professor-list/index.tsx
+++ src/components/Professor-list/index.tsx
@@ -1,15 +1,24 @@
 import Divider from '../Divider';
 import styles from './ProfessorList.module.scss';
+import Image from 'next/image'
 
 export default function ProfessorList({ professores }) {
 
     return (
         <ul className={styles.wrapper}>
             {
-                professores.map((professorNome, i) =>{
+                professores.map((professorNome, i) => {
                     return (
-                        <div key={`${i}_${professorNome}2`}>  
-                            <li>{professorNome}</li>
+                        <div key={`${i}_${professorNome}2`}>
+                            <div className={styles.profile_demo}>
+                                <Image
+                                    width={200}
+                                    height={200}
+                                    alt='Foto do perfil'
+                                    src='/no_image.gif'
+                                />
+                                <li>{professorNome}</li>
+                            </div>
                             <Divider />
                         </div>
                     )
