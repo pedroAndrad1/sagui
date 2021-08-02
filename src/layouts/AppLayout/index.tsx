8194a1784b50515a4dd90bb99a1d81ff4b91ce67
Pedro <pedropaulo3521@gmail.com>
2021-08-02 15:37:34 -0300

    Estilizado modal para carga de históricos

--- src/layouts/AppLayout/index.tsx
+++ src/layouts/AppLayout/index.tsx
@@ -22,6 +22,32 @@ export default function AppLayout({ children }) {
 
     return (
         <>
+            {
+                modalOpen &&
+                <Modal>
+                    <div className =
+                    {
+                    `
+                        ${styles.upload_de_historicos_modal} 
+                        fadeIn
+                    `
+                    }
+                    >
+                        <header>
+                            <h1>Carga de histórico</h1>
+                        </header>
+                        <Divider/>
+                        <main>
+                            Por favor, certifique-se que o conteúdo do pdf está no formato correto.
+                        </main>
+                        <footer>
+                            <label htmlFor="historico" className='button-like'>Carregar</label>
+                            <input name="historico" id="historico" type="file" accept="application/pdf" />
+                            <button onClick={() => closeModal()}>Sair</button>
+                        </footer>
+                    </div>
+                </Modal>
+            }
             <div className={styles.wrapper}>
                 <header>
                     <div className={styles.logo}>
@@ -31,7 +57,7 @@ export default function AppLayout({ children }) {
                         <ul>
                             <Link href='/'><li>Home</li></Link>
                             <Divider direction='vertical' />
-                            <li onClick={() => openModal()}>Upload de históricos</li>
+                            <li onClick={() => openModal()}>Carga de históricos</li>
                             <Divider direction='vertical' />
                             <li>Jubilamento</li>
                             <Divider direction='vertical' />
