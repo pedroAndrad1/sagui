22cd6996e75fe4cc99c0312d8acf522840c1bb72
Pedro de Andrade <dev.pedroandrade@gmail.com>
2022-08-25 16:42:26 -0300

    Retirado import com erro

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@
 // import 'bootstrap/dist/css/bootstrap.css'
 import "font-awesome/css/font-awesome.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "react-tabs/style/react-tabs.scss";
 import "../styles/globals.scss";
 import { UserContextProvider } from "../contexts/UserContext";
 import { ToastContainer } from "react-toastify";
