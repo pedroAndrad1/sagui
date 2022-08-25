6d87906073fac7c788993869fc1a9663365a922d
Pedro de Andrade <dev.pedroandrade@gmail.com>
2022-08-25 20:23:53 -0300

    Adicionada responsividade ao NavBar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,38 +2,62 @@ import styles from './Navbar.module.scss';
 import Link from 'next/link';
 import Divider from '../Divider';
 import { useUserContext } from '../../contexts/UserContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
 
-interface NavbarProps{
+interface NavbarProps {
     openCargaHistoricosModal: () => void;
 }
 
-export default function Navbar( {openCargaHistoricosModal}:NavbarProps ){
+export default function Navbar({ openCargaHistoricosModal }: NavbarProps) {
+    const [screenWidth, setScreenWidth] = useState(null);
 
-    const {logout} = useUserContext();
+    useEffect(() => {
+        setScreenWidth(window.innerWidth);
+        const changeWidth = () => {
+            setScreenWidth(window.innerWidth);
+        }
 
-    return(
+        window.addEventListener('resize', changeWidth)
+
+        return () => {
+            window.removeEventListener('resize', changeWidth)
+        }
+
+    }, [])
+
+    const { logout } = useUserContext();
+    const [openMenuMobile, setOpenMenuMobile] = useState(false);
+
+
+    const toogleMenu = () => {
+        setOpenMenuMobile(!openMenuMobile)
+    }
+
+    return (
         <header className={styles.wrapper}>
-                    <div className={styles.logo}>
-                        <img src="/logo.svg" alt="Sagui logomarca" />
-                    </div>
-                    <nav>
+            <div className={styles.logo}>
+                <img src="/logo.svg" alt="Sagui logomarca" />
+                <FontAwesomeIcon icon={faBars} color='white' onClick={toogleMenu} className={styles.mobile} size="lg" />
+            </div>
+            <nav className={`${styles.navBar}`}>
+                {
+                    openMenuMobile || screenWidth > 800 ?
                         <ul>
                             <Link href='/'><li>Home</li></Link>
-                            <Divider direction='vertical' />
                             <li onClick={() => openCargaHistoricosModal()}>Carga de Históricos</li>
-                            <Divider direction='vertical' />
                             <Link href='/jubilamento'><li>Jubilamento</li></Link>
-                            <Divider direction='vertical' />
                             <Link href='/situacoes-de-atencao'><li>Situações de Atenção</li></Link>
-                            <Divider direction='vertical' />
                             <Link href='/admin'><li>Admin</li></Link>
-                            <Divider direction='vertical' />
                             <li>Sobre</li>
-                            <Divider direction='vertical' />
                             <li onClick={() => logout()}>Logout</li>
                         </ul>
-                    </nav>
-                </header>
+                        :
+                        <></>
+                }
+            </nav>
+        </header>
     )
 
 }
