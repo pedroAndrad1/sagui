5e20f56bfb3c55b620dd3a60184c2316c12e839d
Pedro de Andrade <dev.pedroandrade@gmail.com>
2023-01-24 11:02:13 -0300

    mudança no keycloak

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,16 +1,22 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import KeycloakProvider from "next-auth/providers/keycloak";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
+    // KeycloakProvider({
+    //     clientId:'SaguiTeste',
+    //     clientSecret: '2IeNshUD6z5lzkm2MjAr0BbXfyziuARz',
+    //     issuer: 'http://localhost:8080/realms/myrealm',
+    //   })
     KeycloakProvider({
-        clientId:'SaguiTeste',
-        clientSecret: '2IeNshUD6z5lzkm2MjAr0BbXfyziuARz',
-        issuer: 'http://localhost:8080/realms/myrealm',
-      })
+      clientId: "SaguiApp",
+      clientSecret: "1hc1pUr2OE8lGlZyCVfyZMEMR0yh50OV",
+      issuer: "http://200.156.26.135:8180/realms/sagui",
+    }),
   ],
+  debug: true,
   // callbacks: {
   //   async signIn({ user, account, profile, email, credentials }) {
   //     console.log('Oi')
@@ -20,5 +26,5 @@ export const authOptions = {
   //     return '/login/email-invalido'
   //   }
   // }
-}
-export default NextAuth(authOptions)
+};
+export default NextAuth(authOptions);
